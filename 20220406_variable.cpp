388616ae6fc2b9aea9da118d6cc97f87b22fc353
louisajl <103085834+louisajl@users.noreply.github.com>
2022-04-06 11:08:51 +0800

    Create 20220406_variable.cpp

---
+++ 20220406_variable.cpp
@@ -0,0 +1,17 @@
+//header
+#include <iostream>
+using namespace std;
+int main() {//begin of function
+    float angka;
+    float a;
+    float b;
+    string nama; //deklarasi variabel
+    nama = "Louisa Jesselyne Limanto"; //value dalam variabel
+    angka = 10.21+3.14;
+    a=10.21;
+    b=3.14;
+    cout<< a + b<<endl;
+    cout<<nama<<endl; //memanggil variabel
+    cout<<angka;
+    
+}//end of function
