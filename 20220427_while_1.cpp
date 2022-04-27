9bb22409c044495579a0dc2e47308af235b2acff
louisajl <103085834+louisajl@users.noreply.github.com>
2022-04-27 10:51:20 +0800

    Update 20220427_while_1.cpp

--- 20220427_while_1.cpp
+++ 20220427_while_1.cpp
@@ -12,43 +12,43 @@ int number2 = 0;
      cout<<"2 "<<"*"<<number2<<" = "<<number2*2 <<endl;
     
    }
-int number = 0;
- while( number ++<10){ //perulangan turun
-     cout<<"3 "<<"*"<<number<<" = "<<number*3 <<endl;
+int number3 = 0;
+ while( number3 ++ <10){ //perulangan turun
+     cout<<"3 "<<"*"<<number3<<" = "<<number3*3 <<endl;
     
    }
 int number4 = 0;
- while( number4 ++<10){ //perulangan turun
+ while( number4 ++ <10){ //perulangan turun
      cout<<"4 "<<"*"<<number4<<" = "<<number4*4 <<endl;
     
    }
 int number5 = 0;
- while( number5 ++<10){ //perulangan turun
+ while( number5 ++ <10){ //perulangan turun
      cout<<"5 "<<"*"<<number5<<" = "<<number5*5 <<endl;
     
    }  
 int number6 = 0;
- while( number6 ++<10){ //perulangan turun
+ while( number6 ++ <10){ //perulangan turun
      cout<<"6 "<<"*"<<number6<<" = "<<number6*6 <<endl;
     
    }   
 int number7 = 0;
- while( number7 ++<10){ //perulangan turun
+ while( number7 ++ <10){ //perulangan turun
      cout<<"7 "<<"*"<<number7<<" = "<<number7*7 <<endl;
     
    }
 int number8 = 0;
- while( number8 ++<10){ //perulangan turun
-     cout<<"8 "<<"*"<<number8<<" = "<<number*8 <<endl;
+ while( number8 ++ <10){ //perulangan turun
+     cout<<"8 "<<"*"<<number8<<" = "<<number8*8 <<endl;
     
    }
 int number9 = 0;
- while( number9 ++<10){ //perulangan turun
+ while( number9 ++ <10){ //perulangan turun
      cout<<"9 "<<"*"<<number9<<" = "<<number9*9 <<endl;
     
    }
 int number10 = 0;
- while( number ++<10){ //perulangan turun
+ while( number10 ++ <10){ //perulangan turun
      cout<<"10 "<<"*"<<number10<<" = "<<number10*10 <<endl;
     
    }   
