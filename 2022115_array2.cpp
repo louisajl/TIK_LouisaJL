c72215f4dd9f5cf8020187e267273bd03e8e5a0e
louisajl <103085834+louisajl@users.noreply.github.com>
2022-05-11 10:20:13 +0800

    Create 2022115_array2.cpp

---
+++ 2022115_array2.cpp
@@ -0,0 +1,21 @@
+//array
+#include <iostream>
+using namespace std;
+
+int main(){
+ int data []={
+     123,
+     123,
+     445,
+     5456,
+     67,
+     6678,
+     867,
+     678,
+     6786,
+     678
+ };
+ for (int i = 0; i<10 ; i++)
+ cout<<data[i]<<endl;
+    return 0;
+}
