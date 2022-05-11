1a171007431117f30a4641b59f5bf20859abb6da
louisajl <103085834+louisajl@users.noreply.github.com>
2022-05-11 10:02:57 +0800

    Create 2022115_array.cpp

---
+++ 2022115_array.cpp
@@ -0,0 +1,19 @@
+//array
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ string nama []={
+     "David",
+     "Kabila",
+     "Bryan",
+     "Sidney",
+     "Gracia",
+     "Keysie",
+     "Yoel"
+ };
+ for (int i = 0; i<7 ; i++)
+ cout<<nama[i]<<endl;
+    return 0;
+}
